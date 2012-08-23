c99424cfdc2b77c5ad2e635e363863d9a9b68be7
Je <jetapahob@gmail.com>
2012-08-23 12:29:07 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dating::Application.config.secret_token = '07b9c13aedc296cbe3ed12e423c9d8c7c5d9e1bac42e6a3e2c972c2e52f716e923f056e42d8fd6c67629993bfdce06258546916199c5e6c900aefd7e0abc1e36'
