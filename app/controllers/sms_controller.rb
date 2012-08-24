e7c21eb5782c07b46b5f7b336e5764ef50cbae83
Je <jetapahob@gmail.com>
2012-08-24 11:01:10 +0400

    update

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,8 +1,18 @@
 class SmsController < ApplicationController
   def get
+    if params[:phone] && params[:message]
+      _mess = params[:message].split(/\s/)
+      case _mess[0]
+        when "1"
 
+        when "+"
+
+        else
+
+      end
+    end
   end
-  def send
+  def set
 
   end
 end
